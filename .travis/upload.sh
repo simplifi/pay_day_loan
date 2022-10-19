5d5b9ee0e96329f7f2a17e3b76dec695ded93d7f
Nathan Monteleone <nathan@simpli.fi>
2022-10-19 15:01:39 -0500

    Use the upload.sh from co_mix -- it's correct for a public hex package

--- .travis/upload.sh
+++ .travis/upload.sh
@@ -1,12 +1,12 @@
-  #!/bin/bash
+#!/bin/bash
 
 set -e
 
 # Upload packages to hex
-# Only do so when on master
+# Only do so when a new version is tagged
 
 if [[ ! -z "$TRAVIS_TAG" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-  mix hex.publish --organization simplifi --yes
+  mix hex.publish --yes
 else
   echo "Skipping upload"
 fi
